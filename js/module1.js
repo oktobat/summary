f14c811755f2ae0b28780a5fa10f7f4ce0d9e6fa
oktobat <chamsogum@gmail.com>
2024-02-16 09:34:17 +0900

    2/16일 9시 34분

---
+++ js/module1.js
@@ -0,0 +1,3 @@
+const fnPlusNumbers = (pNum1, pNum2) => pNum1 + pNum2;
+
+export default fnPlusNumbers;
---
+++ js/module2.js
@@ -0,0 +1,17 @@
+const conHello = "안녕하세요";
+const fnPlusNumbers = (pNum1, pNum2) => pNum1 + pNum2;
+
+
+// export default conHello;
+export { conHello, fnPlusNumbers };
+// export { conHello as myConHello, fnPlusNumbers as myFnPlusNumbers };
+
+
+
+// export default 객체명;  대표로 하나만 export 할때
+// import 문에서 새로운 객체를 만들어 옮겨담을 수 있음
+
+// export {객체1, 객체2};  여러개를 export 할때
+// import 문에서도 { 객체1, 객체2 } 동일한 객체명을 사용해야 함
+// 객체명을 변경하려면 export 문에서 as 문 뒤에서 변경하여 내보냄
+// import 문에서 * 로 한꺼번에 받아 as 문 뒤에 대표이름 만들어 사용함
